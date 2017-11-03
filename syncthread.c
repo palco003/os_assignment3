ee8df6ef3fe3e1d1949955a7e02549b46742d2b3
Pablo Alcocer <palco003@fiu.edu>
2017-11-02 21:02:28 -0400

    add header file for system call

--- syncthread.c
+++ syncthread.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include "syncthread.h"
 
 #define PTHREAD_SYNC
 
---
+++ syncthread.h
@@ -0,0 +1,3 @@
+#include<linux/unistd.h>
+#define __NR_print_tasks_pablo_alcocer 343
+extern long int syscall (long int __sysno, ...) __THROW;
